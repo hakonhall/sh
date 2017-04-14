87ba8ae575f289736f9ee2d1af99b0414d75a592
Håkon Hallingstad <hakon@yahoo-inc.com>
2017-04-14 18:09:05 +0200

    makemake now runs tests in priority depending on their source statements

--- main/makemake.sh
+++ main/makemake.sh
@@ -8,6 +8,7 @@ fi
 
 export PATH+=":$PWD/src:$PWD/tests"
 
+source define.sh
 source source_deps.sh
 
 declare -A SRC_DEPS=()
@@ -40,7 +41,21 @@ function Main {
     Map_AddTo SRC_DEPS UNIFIED_DEPS
     Map_AddTo TESTS_DEPS UNIFIED_DEPS
 
-    local -A test_deps=()
+    local -A test_dep_list=()
+    local -A test_dep_map=()
+
+    # If a test T depends on A, B, and C, and another test T2 depends on a
+    # strict subset, e.g. A and B, then make sure the T2 test completes
+    # successfully before trying to run test T.
+    #
+    # We need to map a string (the test file name) to a set (map/associative
+    # array ignoring the values).  More generally, it would be nice to map a
+    # string to any type (string, array, map, or namespace).  Likewise, it
+    # would be nice to allow an array of any type: Allow the elements of array
+    # and map to be any type.
+    #
+    # Can be achieved with 1 level of indirection: The value in the map (or
+    # array) is the name of a variable with the correct type.
 
     local testfile
     for testfile in "${!TESTS_DEPS[@]}"
@@ -48,20 +63,58 @@ function Main {
         TRANSITIVE_DEPS=()
         FindTransitiveDependenciesFor "$testfile"
 
-        test_deps["$testfile"]="${!TRANSITIVE_DEPS[*]}"
+        DefineGlobalVariable -cA
+        local test_dep_varname="$REPLY"
+        local -n test_dep="$test_dep_varname"
+        Map_CopyTo TRANSITIVE_DEPS test_dep
+        test_dep_map["$testfile"]="$test_dep_varname"
+        test_dep_list["$testfile"]="${!TRANSITIVE_DEPS[*]}"
     done
 
-    # If a test T depends on A, B, and C, and another test T2 depends on a
-    # strict subset, e.g. A and B, then make sure the T2 test completes
-    # successfully before trying to run test T.
-
     local test
-    for test in "${!test_deps[@]}"
+    for test in "${!test_dep_map[@]}"
     do
         local prefix="${test%_test.sh}"
-        local dep="${test_deps[$test]}"
-        printf "test/%s/pass: tests/%s_test.sh %s\n" \
-               "$prefix" "$prefix" "$dep"
+        local dep_list="${test_dep_list[$test]}"
+
+        # Find all those tests that should run before ourself: Those tests that
+        # have a strict subset of 'source' dependencies are assumed to test
+        # more basic stuff and should run first.
+
+        local -n test_deps="${test_dep_map[$test]}"
+
+        local -a prerequisite_test_targets=()
+        local -a more_basic_tests=()
+        local other_test
+        for other_test in "${!test_dep_map[@]}"
+        do
+            local -n other_test_deps="${test_dep_map[$other_test]}"
+
+            if (( ${#other_test_deps[@]} < ${#test_deps[@]} ))
+            then
+                local subset=true
+
+                local dep
+                for dep in "${!other_test_deps[@]}"
+                do
+                    if ! test -v test_deps["$dep"]
+                    then
+                        subset=false
+                        break
+                    fi
+                done
+
+                if "$subset"
+                then
+                    local stem="${other_test%_test.sh}"
+                    prerequisite_test_targets+=(test/"$stem"/pass)
+                    more_basic_tests+=("$other_test")
+                fi
+            fi
+        done
+
+        printf "test/%s/pass: tests/%s_test.sh %s %s\n" \
+               "$prefix" "$prefix" "$dep_list" "${prerequisite_test_targets[*]}"
         printf "\ttest_runner.sh \$(FORCE_PASS) $<\n"
         printf "\n"
     done
